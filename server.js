9a20dda6845d27176ccd7ab3149b27ba19d36a1e
Robertas <Robertas@DESKTOP-PSFI987>
2024-06-09 16:00:22 +0300

    added GET endpoint for discovering movies

--- server.js
+++ server.js
@@ -44,6 +44,32 @@ app.post('/genre', async(req, res) => {
 
 });
 
+// New endpoint to get movies based on filters
+
+app.get('/discover-movie', async(req, res) =>{
+    try {
+        const { with_genres, page, vote_average, vote_count, primary_release_date_gte, primary_release_date_lte } = req.query;
+
+        const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+            params: {
+                api_key: process.env.API_KEY,
+                with_genres,
+                page,
+                'vote_average.gte': vote_average,
+                'vote_count.gte': vote_count,
+                'primary_release_date.gte': primary_release_date_gte,
+                'primary_release_date.lte': primary_release_date_lte
+            }
+        });
+
+        res.status(200).json(response.data);
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message});
+    }
+});
+
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
